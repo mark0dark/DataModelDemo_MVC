12436b27983a1e4856b356a0b8f049c3bd54420c
Jianwen <mark_dark2011@126.com>
2013-07-24 09:32:10 +0800

    Initial Commit

---
+++ DataModelDemo/AJAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AJAppDelegate.h
+//  DataModelDemo
+//
+//  Created by Jianwen on 13-7-24.
+//  Copyright (c) 2013年 Dark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class AJViewController;
+
+@interface AJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) AJViewController *viewController;
+
+@end
---
+++ DataModelDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DataModelDemo
+//
+//  Created by Jianwen on 13-7-24.
+//  Copyright (c) 2013年 Dark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AJAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AJAppDelegate class]));
+    }
+}
